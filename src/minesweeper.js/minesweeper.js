a802732e6539dbfe4351787b816d08bcc78ce000
Austin Patterson <pattersonau@msoe.edu>
2022-06-27 02:20:18 -0500

    static genEmptyBoard works!

--- src/minesweeper.js/minesweeper.js
+++ src/minesweeper.js/minesweeper.js
@@ -1,46 +1,55 @@
 export class Minesweeper {
 
+  history = null
+  board = []
+
+
   /**
    * 
    * @param {number || 10} numRows Number of vertical rows
    * @param {number || 10} numCols Number of horizontal columns
    * @param {Array<PlayerAction<{ eventType: ('click' || 'contextMenu'), target: {x: number, y: number}, time: ms}>>} history Array of PlayerAction objects that describe the procedural history of game state. This can be used for Undo & Redo functionality.
    */
-  constructor(numRows, numCols, history = null, boardState = null) {
+  constructor(numRows, numCols, history = null, boardState = []) {
     this.numRows = numRows || 10
     this.numCols = numCols || 10
     this.history = history
-    this.boardState = boardState || genEmptyBoard()
+    this.board = (boardState?.length == numRows && boardState[0]?.length == numCols) || Minesweeper.genEmptyBoard()
   }
 
-}
-
-export function genEmptyBoard(numRows = 10, numCols = 10) {
-  let newBoard = []
-  for (let y = 0; y < numRows; y++) {
-    newBoard[y] = []
-    for (let x = 0; x < numCols; x++) {
-      newBoard[y][x] = null
+  static genEmptyBoard(numRows = 10, numCols = 10) {
+    let newBoard = []
+    for (let y = 0; y < numRows; y++) {
+      newBoard[y] = []
+      for (let x = 0; x < numCols; x++) {
+        newBoard[y][x] = null
+      }
     }
+    return newBoard;
   }
-  return newBoard;
-}
-
-export function revealTile(board, x, y) {
-
-}
-
-export function checkWin(board) {
-
-}
-
-export function checkLose(board) {
-
-}
-
-export function execute(board, playerAction) {
   
+  static revealTile(board, x, y) {
+  
+  }
+  
+  static checkWin(board) {
+  
+  }
+  
+  static checkLose(board) {
+  
+  }
+  
+  static execute(board, playerAction = new PlayerAction()) {
+    
+  }
+
+  initBoard() {
+    this.board
+  }
 }
+
+
 
 
 /*** Helper Classes ***/
@@ -67,6 +76,8 @@ class PlayerAction {
   }
 
   constructor(target = this.default.target, eventType = this.default.eventType, ms = this.default.ms()) {
-
+    this.target = target
+    this.eventType = eventType
+    this.ms = ms
   }
 }
