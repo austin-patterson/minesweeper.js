856b1b289db3f49453d0b373260409064de77d85
Austin Patterson <pattersonau@msoe.edu>
2022-06-27 01:14:17 -0500

    tweaked CSS and minesweeper.vanilla.js until my 10 x 10 grid of *hidden* tiles rendered

--- src/minesweeper.js/minesweeper.vanilla.js
+++ src/minesweeper.js/minesweeper.vanilla.js
@@ -3,7 +3,8 @@ import '../../styles/minesweeper.css'
 // TODO? Consider changing this into a class?
 
 // Variables
-let gameRoot = null,
+let gameRootElement = null,
+    boardElement = null,
     numRows = 10,
     numCols = 10,
     board = []
@@ -16,17 +17,22 @@ export function getBoard() {
 export function genGame(querySelector, rows = 10, cols = 10) {
   console.log('%c[BEGIN] init minesweeper.vanilla.js', "color: #17c6da")
 
-  gameRoot = document.querySelector(querySelector);
-  if (!gameRoot) {
+  gameRootElement = document.querySelector(querySelector);
+  if (!gameRootElement) {
     console.log('%cCould not find an element from the provided query selector: "' + querySelector + '"', "color: red")
     return
   }
 
   numRows = rows
   numCols = cols
-  setRootCss(gameRoot)
+  setRootCss(gameRootElement)
 
-  board = genBoard(numRows, numCols)
+  boardElement = genBoard(numRows, numCols)
+
+  console.log(boardElement)
+
+  // Add children to gameRoot
+  gameRootElement.appendChild(boardElement)
   
   console.log('%c[END] init minesweeper.vanilla.js', "color: #17c6da")
 }
@@ -34,27 +40,56 @@ export function genGame(querySelector, rows = 10, cols = 10) {
 // Helper functions
 function setRootCss(root) {
   root.classList.toggle('minesweeper-game')
-  root.style.setPropertyValue("--var-sizing-factor", 1)
-  root.style.setPropertyValue("--var-grid-height", 16)
-  root.style.setPropertyValue("--var-grid-width", 16)
+  root.style.setProperty("--var-sizing-factor", '1')
+  root.style.setProperty("--var-grid-height", '10')
+  root.style.setProperty("--var-grid-width", '10')
 }
 
-function genBoard(numRows, numCols) {
-  let board = []
+function initBoard(numRows, numCols) {
+  // TODO: board = initBoard() // from minesweeper.js
+
+  let newBoard = []
   for (let y = 0; y < numRows; y++) {
-    board[y] = []
+    newBoard[y] = []
     for (let x = 0; x < numCols; x++) {
-      board[y][x] = genTile()
+      newBoard[y][x] = genTile(x, y)
     }  
   }
 
+  board = [...newBoard]
+
+  // console.log('initBoard() => ', board)
   return board
 }
 
-function genTile() {
+function genBoard(numRows, numCols) {
+  const element = document.createElement('div')
+  element.classList.add('board')
+  element.dataset.numRows = numRows
+  element.dataset.numCols = numCols
+  
+  // console.log(initBoard(numRows, numCols))
+  initBoard(numRows, numCols)
+
+  board.forEach((row, x) => {
+    board[x].forEach((col, y) => {
+      element.appendChild(board[x][y])
+    })
+  })
+
+  boardElement = element
+  return boardElement
+}
+
+function genTile(x, y, state = "hidden") {
   const tile = document.createElement('div')
   tile.classList.add('tile')
-  tile.dataset.state = "hidden"
+  tile.dataset.state = state
+  if (x != null && y != null) {
+    tile.dataset.x = x
+    tile.dataset.y = y
+  }
 
+  // console.log('genTile() => ', tile)
   return tile
 }
