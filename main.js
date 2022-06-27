3b4a6a1a3eca0afbc332197840592818f2180d0b
Austin Patterson <pattersonau@msoe.edu>
2022-06-26 23:30:03 -0500

    more scaffolding, renamed initial file to vanilla.js

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 import './style.css'
 import './src/minesweeper.js/minesweeper'
+import { genGame as genGameAtElement } from './src/minesweeper.js/minesweeper';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// Constants
+
+
+// Functions
+const initApp = () => {
+  document.querySelector('#app').innerHTML = `
+    <h1>Hello Vite!</h1>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+
+    <div id="minesweeper-js"></div>
+  `
+}
+
+
+// Main
+
+function main() {
+  initApp();
+  genGameAtElement('#minesweeper-js');
+}
+
+main()
---
+++ src/minesweeper.js/minesweeper.vanilla.js
@@ -0,0 +1,6 @@
+import '../../styles/minesweeper.css'
+
+export function genGame(querySelector) {
+  let docElement = document.querySelector(querySelector);
+  
+}
