f148f2f1beca7e53a3165f201ce751ee757675a2
Austin Patterson <pattersonau@msoe.edu>
2022-06-27 02:39:56 -0500

    even more scaffolding

--- src/minesweeper.js/minesweeper.js
+++ src/minesweeper.js/minesweeper.js
@@ -1,6 +1,8 @@
 export class Minesweeper {
-
+  // Array of PlayerAction objects that describe the procedural history of game state. This can be used for Undo & Redo functionality.
   history = null
+  
+  //  board[x][y]: { -1: mine, 0-8: number of mines in adjacent tiles }
   board = []
 
 
@@ -8,14 +10,17 @@ export class Minesweeper {
    * 
    * @param {number || 10} numRows Number of vertical rows
    * @param {number || 10} numCols Number of horizontal columns
-   * @param {Array<PlayerAction<{ eventType: ('click' || 'contextMenu'), target: {x: number, y: number}, time: ms}>>} history Array of PlayerAction objects that describe the procedural history of game state. This can be used for Undo & Redo functionality.
+   * @param {Array<PlayerAction>>} history Array of PlayerAction objects that describe the procedural history of game state. This can be used for Undo & Redo functionality.
    */
-  constructor(numRows, numCols, history = null, boardState = []) {
+  constructor(numRows, numCols, numMines, history = null, boardState = []) {
     this.numRows = numRows || 10
     this.numCols = numCols || 10
+    this.numMines = numMines || 15
     this.history = history
     this.board = (boardState?.length == numRows && boardState[0]?.length == numCols) || Minesweeper.genEmptyBoard()
   }
+
+  /*** Static functions ***/
 
   static genEmptyBoard(numRows = 10, numCols = 10) {
     let newBoard = []
@@ -29,27 +34,58 @@ export class Minesweeper {
   }
   
   static revealTile(board, x, y) {
-  
+    recursiveRevealTile(board, x, y)
   }
   
-  static checkWin(board) {
+  // static checkWin(board) {
+  
+  // }
+  
+  // static checkLose(board) {
+
+  // }
+
+  static isMine(board, x, y) {
+    return board[x][y] === 0
+  }
+
+  static genScore(board, time, numActions)
+
+
+  /*** Public functions ***/
+  
+  // Reveals tile at board[x][y]. May cause a chain reaction revealing adjacent tiles, and so on.
+  reveal(x, y) {}
+
+  // Toggles flag at board[x][y]
+  flag(x, y) {}
+
+  // Returns the current score.
+  getScore() {}
+
+  // Returns the "current" time on the timer in ms.
+  getTime() {}
+  
+  /*** Private functions ***/
+
+  // Fills the board with mines and numbers.
+  initBoard(numMines) {
   
   }
-  
-  static checkLose(board) {
-  
+
+  selectRandomPosition(rowLength, columnLength) {
+    return { x: Math.random() * rowLength, y: Math.random() * columnLength }
   }
-  
-  static execute(board, playerAction = new PlayerAction()) {
+
+  recursiveRevealTile(board, x, y) {
+
+  }
+
+  execute(board, playerAction = new PlayerAction()) {
     
   }
-
-  initBoard() {
-    this.board
-  }
+  
 }
-
-
 
 
 /*** Helper Classes ***/
